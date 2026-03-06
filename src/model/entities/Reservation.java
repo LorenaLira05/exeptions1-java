d536f725d2d3c4336307b7bc0add32fdee76e791
Lorena Lira <naheelee2005@gmail.com>
2026-03-06 20:21:06 -0300

    very bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
